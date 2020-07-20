347e6675ea07577b36a44f7babf51eb0fcbf2c8e
Güven Atbakan <shibby@users.noreply.github.com>
2020-07-20 23:00:09 +0300

    make trash list obvious and easy editable

--- background.js
+++ background.js
@@ -1,6 +1,37 @@
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    var q = text +  " -site:trthaber.com -site:memurlar.net -site:yeniasir.com.tr -site:t24.com.tr -site:haber7.com -site:sondakika.com -site:yenisafak.com -site:posta.com.tr -site:milligazete.com.tr -site:birgun.net -site:yeniakit.com.tr -site:internethaber.com -site:ensonhaber.com -site:mynet.com -site:star.com.tr -site:takvim.com.tr -site:gazeteduvar.com.tr -site:haberturk.com -site:sozcu.com.tr -site:haberler.com -site:odatv.com -site:hurriyet.com.tr -site:milliyet.com.tr -site:sabah.com.tr -site:cumhuriyet.com.tr -site:cnnturk.com";
+    var trash = [
+      'trthaber.com',
+      'memurlar.net',
+      'yeniasir.com.tr',
+      't24.com.tr',
+      'haber7.com',
+      'sondakika.com',
+      'yenisafak.com',
+      'posta.com.tr',
+      'milligazete.com.tr',
+      'birgun.net',
+      'yeniakit.com.tr',
+      'internethaber.com',
+      'ensonhaber.com',
+      'mynet.com',
+      'star.com.tr',
+      'takvim.com.tr',
+      'gazeteduvar.com.tr',
+      'haberturk.com',
+      'sozcu.com.tr',
+      'haberler.com',
+      'odatv.com',
+      'hurriyet.com.tr',
+      'milliyet.com.tr',
+      'sabah.com.tr',
+      'cumhuriyet.com.tr',
+      'cnnturk.com',
+    ].map(function(site) {
+      return '-site:' + site;
+    });
+
+    var q = text +  " " + trash.join(' ');
     var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(q);
     chrome.tabs.create({ url: newURL });
   });
